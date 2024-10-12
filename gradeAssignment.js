256ee96dc7cdd7cb045f06731d5de21b65961f10
kulotskiI <ishielmishma24@gmail.com>
2024-10-12 17:36:04 +0800

    Initial commint

---
+++ gradeAssignment.js
@@ -0,0 +1,10 @@
+const grade = 69;
+if (grade <= 70) {
+    console.log ("Needs Imrovement")
+} else if (grade >= 80 ) {
+    console.log ("Good")
+} else if (grade >= 70) {
+    console.log("Fair")
+} else {
+    console.log("Excellent")
+}
